5c1e767a41d1d5de593d90a51d8986d90faa5cdd
Glauber J. Silva <gjsilva83@gmail.com>
2024-02-09 22:16:23 -0300

    remocao de lixo

--- java/com/glauber/gfood/domain/repository/PermissaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.glauber.gfood.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.glauber.gfood.domain.model.Permissao;
-
-public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
-
-}
